f437e562eb79dc50dbae85e1b4dd5b502d5e77e3
labush <aestdata@gmail.com>
2022-02-17 10:25:50 +0100

    1 : show/hide div

---
+++ hide_click/index.js
@@ -0,0 +1,26 @@
+const showLink = document.querySelector('#showLink');
+showLink.addEventListener('click', () => {
+    document.querySelector('.content_hide').style.display = 'block';
+})
+const hideLink = document.querySelector('#hideLink');
+hideLink.addEventListener('click', () => {
+    document.querySelector('.content_hide').style.display = 'none';
+})
+
+const showLink1 = document.querySelector('#showLink1');
+showLink1.addEventListener('click', () => {
+    document.querySelector('.content_hide1').style.display = 'block';
+})
+const hideLink1 = document.querySelector('#hideLink1');
+hideLink1.addEventListener('click', () => {
+    document.querySelector('.content_hide1').style.display = 'none';
+})
+
+const showLink2 = document.querySelector('#showLink2');
+showLink2.addEventListener('click', () => {
+    document.querySelector('.content_hide2').style.display = 'block';
+})
+const hideLink2 = document.querySelector('#hideLink2');
+hideLink2.addEventListener('click', () => {
+    document.querySelector('.content_hide2').style.display = 'none';
+})
