3cd07ac055b04bfcba69ff8343dfb27b3c5797ff
labush <aestdata@gmail.com>
2022-02-18 09:34:38 +0100

    2 : navbar_hide_scroll

---
+++ navbar_hide/index.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY); // valeur en pixel de ce qui a été scrollé
+
+    if (window.scrollY > 145) {
+        nav.style.marginTop = '0px';
+    }
+    else if (window.scrollY < 145) {
+        nav.style.marginTop = '-50px';
+    }
+})
